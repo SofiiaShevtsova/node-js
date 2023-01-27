1351d2bf59c077571e570c7964f57e3bdb421cda
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-01-27 19:27:51 +0200

    do all

---
+++ contacts.js
@@ -0,0 +1,74 @@
+const { nanoid } = require("nanoid");
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.resolve(`./db/contacts.json`);
+
+const listContacts = async () => {
+  try {
+    const list = await fs.readFile(contactsPath, "utf-8");
+
+    const listOfContacts = JSON.parse(list);
+    console.log(listOfContacts);
+    return listOfContacts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const listOfContacts = await listContacts();
+    const contact = listOfContacts.find((elem) => elem.id === `${contactId}`);
+    console.log(contact);
+    return contact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const listOfContacts = await listContacts();
+    console.log(
+      `Видаляємо:${listOfContacts.find((elem) => elem.id === `${contactId}`)}`
+    );
+
+    const newListOfContacts = listOfContacts.filter(
+      (elem) => elem.id !== `${contactId}`
+    );
+    console.log(newListOfContacts);
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(newListOfContacts, null, 2)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addContact = async (name, email, phone) => {
+  try {
+    const listOfContacts = await listContacts();
+    listOfContacts.push({
+      name: name,
+      email: email,
+      phone: phone,
+      id: nanoid(),
+    });
+    const newListOfContacts = listOfContacts;
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(newListOfContacts, null, 2)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
